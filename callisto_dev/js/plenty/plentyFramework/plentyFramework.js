9f851af4633413932d69ca235dd243b62d0b00ba
Felix <felix.dausch@plentymarkets.com>
2015-07-01 11:16:05 +0200

    extend PlentyFramework giving directives additional dependencies to inject services in directives

--- callisto_dev/js/plenty/plentyFramework/plentyFramework.js
+++ callisto_dev/js/plenty/plentyFramework/plentyFramework.js
@@ -6,6 +6,12 @@
      */
     PlentyFramework = function() {};
 
+    var instance = null;
+    PlentyFramework.getInstance = function() {
+        instance = instance || new PlentyFramework();
+        return instance;
+    };
+
     /**
      * @type {Array} Collection of registered directives
      */
@@ -15,14 +21,16 @@
      * Register directive. Directives can be bound to dynamically added nodes by calling pm.bindPlentyFunctions();
      * @param   {string}    selector        jQuery selector of the DOM-elements to bind the directive to
      * @param   {function}  callback        Function to add directives behaviour
+     * @param   {Array}     dependencies    List of required services. Services will be passed to callback function
      * @param   {boolean}   allowDuplicates Defines if a directive can be bound to the same element multiple times
      * @returns {object}                    The created directive
      */
-    PlentyFramework.directive = function(selector, callback, allowDuplicates) {
+    PlentyFramework.directive = function(selector, callback, dependencies, allowDuplicates) {
         var directive = {
             id:			PlentyFramework.directives.length,
             selector:	selector,
             callback:	callback,
+            dependencies: dependencies,
             allowDuplicates: !!allowDuplicates,
             elements:	[]
         };
@@ -46,7 +54,24 @@
                 }
             });
             if( !directiveSelector || directiveSelector === directive.selector || $.inArray(directive.selector, directiveSelector ) >= 0 ) {
-                $.each( elements, directive.callback );
+                $.each( elements, function(i, elem) {
+                    var params = [i, elem];
+
+                    // append dependencies if exists
+                    if( !!directive.dependencies && directive.dependencies.length > 0 ) {
+                        $.each( directive.dependencies, function(j, dependency) {
+                            var service = PlentyFramework.service(dependency);
+                            if( !service ) {
+                                console.error('Cannot inject service "' + dependency + "': Service not found.");
+                            }
+                            params.push( service );
+                        });
+                    }
+
+                    // apply loop variables and dependend services to callback
+
+                    directive.callback.apply(null, params);
+                });
             }
 
             $(elements).each(function(j, obj) {
@@ -60,6 +85,31 @@
 
     PlentyFramework.service = function( serviceName, serviceFunctions ) {
 
+
+        // Catch type mismatching for 'serviceName'
+        if( typeof serviceName !== 'string' ) {
+            console.error("Type mismatch: Expect first parameter to be a 'string', '" + typeof serviceFunctions + "' given.");
+            return;
+        }
+
+        // getting services
+        if( typeof serviceFunctions === 'undefined' ) {
+            return PlentyFramework.prototype[serviceName];
+        }
+
+        // Catch type mismatching for 'serviceFunctions'
+        if( typeof serviceFunctions !== 'function' ) {
+            console.error("Type mismatch: Expect second parameter to be a 'function', '" + typeof serviceFunctions + "' given.");
+            return;
+        }
+
+        // Avoid overriding existing services
+        if( PlentyFramework.prototype.hasOwnProperty( serviceName ) ) {
+            console.error("Service '" + serviceName + "' already exists.");
+            return;
+        }
+
+        // inject service
         PlentyFramework.prototype[serviceName] = serviceFunctions();
 
     };
@@ -67,7 +117,7 @@
 }(jQuery));
 
 // Create global instance of PlentyFramework for usage in Webshop-Layouts
-var pm = new PlentyFramework();
+var pm = PlentyFramework.getInstance();
 
 // initially bind all registered directives
 jQuery(document).ready(function() {
