18a722a02af2f4e56b4e4f945f9ee423c3c34687
Felix <felix.dausch@plentymarkets.com>
2015-07-01 15:41:15 +0200

    Fix redirectHref directive by injecting MediaSize Service instead of using window.mediaSize

--- callisto_dev/js/plenty/plentyFramework/directives/redirectHref.js
+++ callisto_dev/js/plenty/plentyFramework/directives/redirectHref.js
@@ -1,17 +1,17 @@
 (function($, pm) {
 
     // link non-anchor elements
-    pm.directive('a[data-plenty-href]', function(i, elem) {
+    pm.directive('a[data-plenty-href]', function(i, elem, MediaSize) {
         $(elem).each(function() {
             var href = $(this).attr('href');
             var identifier = $(this).attr('data-plenty-href');
 
             $('[data-plenty-link="'+identifier+'"]').click(function() {
-                if( window.mediaSize != 'xs' ) {
+                if( MediaSize.interval() != 'xs' ) {
                     window.location.href = href;
                 }
             });
         });
-    });
+    }, ['MediaSize']);
 
 }(jQuery, PlentyFramework));
